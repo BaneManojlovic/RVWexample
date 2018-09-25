04c5857396c553d7f743bb48456a310e5307d9fc
Branislav Manojlovic <bane1manojlovic@gmail.com>
2018-09-25 15:08:28 +0200

    Second commit.

--- app/src/main/java/com/bitnovisad/rvwexample/main/view/MainActivity.java
+++ app/src/main/java/com/bitnovisad/rvwexample/main/view/MainActivity.java
@@ -1,13 +1,21 @@
-package com.bitnovisad.rvwexample;
+package com.bitnovisad.rvwexample.main.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.bitnovisad.rvwexample.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize recyclerView object
+        recView = (RecyclerView) findViewById(R.id.playerRecViewList);
     }
 }
